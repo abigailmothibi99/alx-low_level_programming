2ad6e4314826682d937f0d02a85d57c447ac8bb3
abigailmothibi99 <abigailmothibi99@icloud.com>
2023-06-15 08:19:21 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+0x00. C - Hello, World
+2023/06/15, 09:33
+Page 1 of 4
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+}
