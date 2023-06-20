002f5a038f0afb000e7a6cfddb6e504dbb69d096
abigailmothibi99 <abigailmothibi99@icloud.com>
2023-06-20 07:45:13 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,8 @@
-int _putchar(char c); 
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
